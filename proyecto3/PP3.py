01bbead102da2988c3f2bca563d0a3bed3193ca5
AmpiePablo <39846644+AmpiePablo@users.noreply.github.com>
2018-06-01 21:14:20 -0600

    tercer commit

--- proyecto3/PP3.py
+++ proyecto3/PP3.py
@@ -2,6 +2,30 @@ from tkinter import *
 from PIL import Image,ImageTk
 import time
 import random
+import pygame
+
+
+#E:recibe la cancion
+#S:retorna la cancion escogida 
+#R:debe recibir la cancion en string
+def play(cancion):#Esta funcion permite cargar una cancion poder reproducirla la libreria que se utilizo fue pygame ya que windsound no ejecutaba a la misma vez que la interfaz
+    pygame.mixer.init()
+    pygame.mixer.music.load(cancion)
+    pygame.mixer.music.play(-1)#el -1 esta ya que esto permite que la cancion no se detenga
+ #-------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#E:----ninguno---------
+#S: pause la musica
+#R:---ninguno---------
+def pause():#esta funcion permite detener la cancion si el usuario ya no desea escucharla mas
+    pygame.mixer.music.pause()
+ #-------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#E:----ninguno---------
+#S: continua la musica
+#R:---ninguno---------
+def continuar():#esta funcion permite seguir con la cancion 
+    pygame.mixer.music.unpause()
+
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 #E:recibe la imagen o label que desea realizar la animacion
 #S:retorna una animacion del logo dado sumando al eje y
@@ -34,8 +58,13 @@ img_musica=PhotoImage(file="imagenes_programa/suena.png")
 lbl_logo=Label(ventana_principal,image=img_titulo,bg="#445a6d")
 lbl_logo.place(x=320,y=500)
 #botones de musica
-#btn_sigue=Button(ventana_principal)
-#btn_pausa=Button(ventana_principal)
+btn_sigue=Button(ventana_principal,image=img_musica,bg="#445a6d",bd=0,highlightthickness=0,activebackground="#445a6d",command=lambda:continuar())
+btn_sigue.place(x=5,y=20)
+btn_pausa=Button(ventana_principal,image=img_mute,bg="#445a6d",bd=0,highlightthickness=0,activebackground="#445a6d",command=lambda:pause())
+btn_pausa.place(x=5,y=120)
+btn_jugar=Button(ventana_principal,image=img_play,bg="#445a6d",bd=0,highlightthickness=0,activebackground="#445a6d")
+btn_jugar.place(x=600,y=450)
+#play("laita.mp3")
 
 #----------------------------------------------
 animacion(lbl_logo,-20,15,500)
